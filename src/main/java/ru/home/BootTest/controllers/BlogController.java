282365fc333b20a909609f3b954385ba47a9359e
oleg <holllor@mail.ru>
2020-09-14 14:21:04 +0300

    выполнен 6 и 7 урок

--- src/main/java/ru/home/BootTest/controllers/BlogController.java
+++ src/main/java/ru/home/BootTest/controllers/BlogController.java
@@ -3,9 +3,12 @@ package ru.home.BootTest.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.home.BootTest.models.Post;
 import ru.home.BootTest.repo.PostRepository;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -14,12 +17,69 @@ public class BlogController {
     private PostRepository postRepository;
 
     @GetMapping("/blog")
-    public String blogMain(Model model){
+    public String blogMain(Model model) {
         Iterable<Post> posts = postRepository.findAll();
-        model.addAttribute("posts",posts);
+        model.addAttribute("posts", posts);
         return "blog-main";
     }
 
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        //  Iterable<Post> posts = postRepository.findAll();
+        //  model.addAttribute("posts",posts);
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,
+                              @RequestParam String anons,
+                              @RequestParam String full_text,
+                              Model model) {
+        Post post = new Post(title, anons, full_text);
+
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if (postRepository.existsById(id)) {
+            Optional<Post> post = postRepository.findById(id);
+            ArrayList<Post> res = new ArrayList<>();
+            post.ifPresent(res::add);
+            model.addAttribute("post", res);
+            return "blog-details";
+        } else {
+            return "redirect:/blog";
+        }
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model) {
+        if (postRepository.existsById(id)) {
+            Optional<Post> post = postRepository.findById(id);
+            ArrayList<Post> res = new ArrayList<>();
+            post.ifPresent(res::add);
+            model.addAttribute("post", res);
+            return "blog-edit";
+        } else {
+            return "redirect:/blog";
+        }
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id,
+                                 @RequestParam String title,
+                                 @RequestParam String anons,
+                                 @RequestParam String full_text,
+                                 Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
 }
 
 
--- src/main/java/ru/home/BootTest/models/Post.java
+++ src/main/java/ru/home/BootTest/models/Post.java
@@ -7,6 +7,16 @@ import javax.persistence.Id;
 
 @Entity
 public class Post {
+
+    public Post() {
+           }
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
